90f1cfc08ff7d8d4c77c3a731d8c9c348bed8e79
德华 刘 <gavinprior5@gmail.com>
2024-06-11 23:19:17 +0000

    update logo component to render image

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import JessieLewis from "../images/JessieLewis.png";
 
 const Logo = ({ height = 45 }) => {
   return (
     <NavLink to="/">
       <img
-        src="/JessieLewis.png"
+        src={JessieLewis}
         alt="Logo for Jessie Lewis Mental Performance Consulting"
         height={height}
         width={height}
