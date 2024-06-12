6566d923a3a0018e399cc41c5d5762074d05fe33
德华 刘 <gavinprior5@gmail.com>
2024-06-12 02:33:04 +0000

    changing the homepage key value to relove deployment issues

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const Root = () => {
 // --- create variable called 'router'
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route path="/jessielewis" element={<Root />}>
       <Route index element={<HomePage />} />
       <Route exact path="/about" element={<AboutMe />} />
       <Route exact path="/services" element={<Services />} />
