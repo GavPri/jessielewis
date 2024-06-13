a49de41cb252ac1796ad536669fa3ca8a21d0d0e
德华 刘 <gavinprior5@gmail.com>
2024-06-13 21:47:25 +0000

    wrap app in hash router in index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 // ---- import hashrouter
 import { HashRouter } from "react-router-dom";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <HashRouter>
       <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
