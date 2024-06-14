64cd85e7a5f0ee2c00f4b957daf4a1587845f9fe
德华 刘 <gavinprior5@gmail.com>
2024-06-14 00:39:12 +0000

    remove fade in import in about me.js

--- src/pages/about-me/AboutMe.js
+++ src/pages/about-me/AboutMe.js
@@ -2,7 +2,7 @@ import React from "react";
 import jessiebanner from "../../images/jessiebanner.png";
 import AboutMeCard from "./AboutMeCard";
 import { FaArrowAltCircleDown } from "react-icons/fa";
-import FadeIn from "../../components/FadeInAnimation";
+
 
 const AboutMe = () => {
   // ---- Profile picture
@@ -24,7 +24,7 @@ const AboutMe = () => {
           className="h-dvh w-dvw aspect-square rounded-lg m-auto mx-2"
         >
           {/* --- scroll indicator */}
-          <FadeIn>
+          
             <div className="opacity-0 w-64 aspect-square mt-auto bg-dark mx-auto absolute bottom-32 left-0 right-0 flex justify-center flex-col items-center rounded-md">
               <p className="mb-12 font-poppins text-light text-3xl">About Me</p>
               <FaArrowAltCircleDown
@@ -32,7 +32,7 @@ const AboutMe = () => {
                 className="animate-bounce text-light"
               />
             </div>
-          </FadeIn>
+          
         </div>
       </div>
       <AboutMeCard />
